8e8f09144661c68d111f79789f669ddb5ca40001
Ti-Fen Pan <tpan@squareup.com>
2017-03-30 10:18:35 -0700

    customer api tests

--- test/test_customer_api.py
+++ test/test_customer_api.py
@@ -26,37 +26,80 @@ import unittest
 import squareconnect
 from squareconnect.rest import ApiException
 from squareconnect.apis.customer_api import CustomerApi
+from squareconnect.models.customer import Customer
+
+from .utils import APITestCase
 
 
-class TestCustomerApi(unittest.TestCase):
+class TestCustomerApi(APITestCase):
     """ CustomerApi unit test stubs """
 
     def setUp(self):
         self.api = squareconnect.apis.customer_api.CustomerApi()
+        self.account = self.accounts['US-Prod-Sandbox']
+        self.access_token = self.account['access_token']
 
     def tearDown(self):
         pass
 
-    def test_create_customer(self):
-        print ("Start test case for create_customer")
-        pass
+    def test01_create_customer(self):
+        print ('Start test case for create_customer')
 
-    def test_delete_customer(self):
-        print ("Start test case for delete_customer")
-        pass
+        body = {
+            'given_name':"Guido", 
+            'family_name':"Rossum", 
+            'reference_id':"python21"
+        }
+        resp = self.api.create_customer(self.access_token, body)
+        self.assertIsNone(resp.errors, "Errors in the result.")
 
-    def test_list_customers(self):
-        print ("Start test case for list_customers")
-        pass
+        self.__class__.customer = resp.customer
+        expected_customer = Customer(given_name="Guido", family_name="Rossum", reference_id="python21")
+        self.validate_customer_equality(resp.customer, expected_customer)
 
-    def test_retrieve_customer(self):
-        print ("Start test case for retrieve_customer")
-        pass
+    def test02_retrieve_customer(self):
+        print ('Start test case for retrieve_customer')
 
-    def test_update_customer(self):
-        print ("Start test case for update_customer")
-        pass
+        customer_id = self.__class__.customer.id
+        response = self.api.retrieve_customer(self.access_token, customer_id)
+        self.assertIsNone(response.errors)
+   
+    def test03_update_customer(self):
+        print ('Start test case for update_customer')
 
+        customer_id = self.__class__.customer.id
+        body = {'given_name': "Van"}
+        response = self.api.update_customer(self.access_token, customer_id, body)
+        self.assertIsNone(response.errors)
+
+        expected_customer = Customer(given_name="Van", family_name="Rossum", reference_id="python21")
+        self.validate_customer_equality(response.customer, expected_customer)
+
+    def test04_list_customers(self):
+        print ('Start test case for list_customers')
+        
+        response = self.api.list_customers(authorization=self.access_token)
+        self.assertIsNone(response.errors)
+
+        # Second request with previous cursor
+        if response.cursor is not None:
+          response = self.api.list_customers(authorization, cursor=response.cursor)
+          self.assertIsNone(response.errors)
+
+    def test07_delete_customer(self):
+        print ('Start test case for delete_customer')
+        customer_id = self.__class__.customer.id
+        response = self.api.delete_customer(self.access_token, customer_id)
+        self.assertIsNone(response.errors)
+
+
+    def validate_customer_equality(self, actual, expected):
+        self.assertEqual(actual.given_name, expected.given_name, 
+          "expected: %s ,but got: %s " % (expected.given_name, actual.given_name))
+        self.assertEqual(actual.family_name, expected.family_name, 
+          "expected: %s ,but got: %s " % (expected.family_name, actual.family_name))
+        self.assertEqual(actual.reference_id, expected.reference_id, 
+          "expected: %s ,but got: %s " % (expected.reference_id, actual.reference_id))
 
 if __name__ == '__main__':
     unittest.main()
